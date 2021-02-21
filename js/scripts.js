c79708be46e0f3b8bd4cb296a52b040219ac35d4
estef1207 <estefany.16.santamaria@gmail.com>
2021-02-21 20:20:00 +0100

    Y por fin hacemos sonar el Dombo, adecuacion del codigo del JS, identificadores en los botones y en los sonidos para enlazar el JS, falta hacer que se interrumpa e inicie sin importar el tiempo o numero de veces y rapides del click

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+var son1 = document.getElementById("bombo"); 
+var son2 = document.getElementById("crash"); 
+var son3 = document.getElementById("hit"); 
+var son4 = document.getElementById("redo"); 
+
+function playAudio1() { 
+  son1.play(); 
+}
+function playAudio2() { 
+   son2.play(); 
+ }
+ function playAudio3() { 
+   son3.play(); 
+ }
+ function playAudio4() { 
+   son4.play(); 
+ } 
